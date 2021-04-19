9f79192c56184abe1eb28db277bfe3c2e911443f
古昊 <78775699+HaoLikesCars@users.noreply.github.com>
2021-04-19 14:32:20 -0700

    Update nd.c
    added more stuff

--- nd.c
+++ nd.c
@@ -1,7 +1,8 @@
 double kp;
 double kd;
 //integral not useful
-
+double dp; //accounting for drift with encoder values
+double gp; //accounting for drift with a gyro
 //Universal variables
 double lerror;
 double rerror;
@@ -10,26 +11,47 @@ double terror;
 double lPrevError;
 double rPrevError;
 
-double lproportional;
-double rporportional;
 double lderivative;
 double rderivative;
 
+void setconstants(double p,double d,double df,double g)//makes tuning easier in the main document
+{
+  kp=p;
+  kd=d;
+  dp=df;
+  gp=g;
+}
+double drift(double l,double r)
+{
+  return (l-r)*dp;
+}
 void drive(double target)
 {
+  resetMotorEncoder(right);
+	resetMotorEncoder(left);
   lerror=target;
   rerror=target;
   lPrevError=lerror;
   rPrevError=rerror;
-  
   while(abs(lerror)>2||abs(lderivative)>1)
   {
     sleep(20);
     lerror=target-getMotorEncoder(Left);
     rerror=target-getMotorEncoder(Right);
-    
-    
-    
+    lderivative=lerror-lprevError;
+    rderivative=rerror-rprevError;
+    lprevError=lerror;
+    rprevError=rerror;
+    terror = angle-getGyroHeadingFloat(gyro);
+		if (terror > 180){
+			terror = terror - 360;
+		}
+		else if (terror < -180){
+			terror = terror + 360;
+		}
+    setMotorSpeed(Left,error*kp+lderivative*kd-terror*gp);
+    setMotorSpeed(Right,error*kp+rderivative*kd-terror*gp);
   }
-  
+  stopMotor(Left);
+  stopMotor(Right);
 }
