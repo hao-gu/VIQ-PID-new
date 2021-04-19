cc866d2c22963fa02a506ab22d0ea7e61bd48b82
古昊 <78775699+HaoLikesCars@users.noreply.github.com>
2021-04-19 14:06:29 -0700

    Update nd.c

--- nd.c
+++ nd.c
@@ -1,1 +1,35 @@
-fsdfsdfs
+double kp;
+double kd;
+//integral not useful
+
+//Universal variables
+double lerror;
+double rerror;
+double terror;
+
+double lPrevError;
+double rPrevError;
+
+double lproportional;
+double rporportional;
+double lderivative;
+double rderivative;
+
+void drive(double target)
+{
+  lerror=target;
+  rerror=target;
+  lPrevError=lerror;
+  rPrevError=rerror;
+  
+  while(abs(lerror)>2||abs(lderivative)>1)
+  {
+    sleep(20);
+    lerror=target-getMotorEncoder(Left);
+    rerror=target-getMotorEncoder(Right);
+    
+    
+    
+  }
+  
+}
